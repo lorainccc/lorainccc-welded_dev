406515a5897bebc855d8c81f321f0c9bc24d6cd4
davebrogan <dave@davebrogan.com>
2022-02-23 22:58:12 -0700

    finished wrokshops page

--- page-workshops.php
+++ page-workshops.php
@@ -6,8 +6,25 @@
  * @package lorainccc-welded
  */
 
-get_header(); ?>
+get_header(); 
+?>
 
-<section>
-    <?php get_template_part( 'template-parts/content', 'banner' ); ?>
-</section>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+    <header class="entry-header workshops-header">
+        <?php get_template_part( 'template-parts/content', 'banner' ); ?>
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+    <div class="entry-content">
+		<div class="main-content-section">
+			<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'lccc-framework' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div>
+	</div><!-- .entry-content -->
+</article>
+<?php get_footer(); ?>
